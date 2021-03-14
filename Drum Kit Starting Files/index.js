c00b70439d418fe4de9ada993e3eae77d8301a07
Kyle Chu <kylewchu@gmail.com>
2021-03-14 12:49:41 -0400

    Drumkit event listeners and sounds

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,14 @@
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+
+
+
+    });
+}
+
+
+
+var audio = new Audio("sounds/");
+audio.play();
